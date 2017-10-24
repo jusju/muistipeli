bdd8fd8282389c8542ced098c4010f45a321a648
Juslin Jukka <jusju@haaga-helia.fi>
2017-10-24 13:11:59 +0300

    gridin teko

--- src/com/juslin/muistipeli/kayttoliittyma/FrPaaNaytto.java
+++ src/com/juslin/muistipeli/kayttoliittyma/FrPaaNaytto.java
@@ -1,7 +1,9 @@
 package com.juslin.muistipeli.kayttoliittyma;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -9,56 +11,51 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.juslin.tyokalut.GenericRoundedButton;
 import com.juslin.tyokalut.MuistiPainike;
 
 public class FrPaaNaytto extends JFrame implements ActionListener{
 
-	private ArrayList<JButton> painikkeet = new ArrayList<JButton>();
-	private JButton[][] painikeTaulukko = new JButton[8][8];
+	private ArrayList<JButton> kortit = new ArrayList<JButton>();
+	private JButton btKortti = null;
+	private JPanel paKortit = new JPanel(new GridLayout(6,6));
 	
 	public FrPaaNaytto() {
-		setSize(400, 400);
+		setSize(600, 600);
 		setLocation(200, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ImageIcon img = new ImageIcon("kuvat/icon.png");
 		setIconImage(img.getImage());
 		setTitle("Muistipeli");
+		for (int i = 0; i < 36; i++) {
+			btKortti = new JButton();
+			btKortti.addActionListener(this);
+			kortit.add(btKortti);
+		}
+		
+		setResizable(false);
+		setVisible(true);
 		asetteleKomponentit();
 	}
 	
 	public void asetteleKomponentit() {
 		Container sisalto = this.getContentPane();
-		sisalto.setLayout(new FlowLayout());
-		int xKoordinaatit = 0;
-		int yKoordinaatit = 0;
-		for (int i = 0; i < 64; i++) {
-			GenericRoundedButton btUusiPainike = new GenericRoundedButton("X");
-			painikeTaulukko[xKoordinaatit][yKoordinaatit] =  btUusiPainike;
-			xKoordinaatit++;
-			if(xKoordinaatit == 7) {
-				yKoordinaatit++;
-				if(yKoordinaatit == 8) {
-					sisalto.add(btUusiPainike);
-					painikeTaulukko[7][7] = btUusiPainike;
-					break;
-				}
-				xKoordinaatit = 0;
-			}
-			sisalto.add(btUusiPainike);
+		sisalto.setLayout(new BorderLayout());
+		
+		for (int i = 0; i < kortit.size(); i++) {
+			paKortit.add(kortit.get(i));
 		}
+		sisalto.add(paKortit);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+				
 	}
 	
 	public static void main(String[] args) {
 		FrPaaNaytto naytto = new FrPaaNaytto();
-		naytto.setVisible(true);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		
 	}
 
 }
